10dcc0c917d0cf4eaed7e3c930a1f62359c8c0d3
FelipeGallego6 <felipegallego6@gmail.com>
2023-03-16 12:26:37 -0500

    ok suma.js

---
+++ js-1/js-2/suma.js
@@ -0,0 +1,10 @@
+function sum(arr) {
+  let sumaElementos = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumaElementos = sumaElementos + arr[i];
+  }
+  return sumaElementos;
+}
+console.log(sum([1, 2, 3])); // 6
+console.log(sum([10, 8, 12, 5])); // 35
+console.log(sum([3, 3, 3, 3, 3, 3])); // 0
